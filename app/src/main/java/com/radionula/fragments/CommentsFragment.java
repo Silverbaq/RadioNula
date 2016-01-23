1c35bdf575da5ada183e70728ea1409edbb21321
Steffen Mogensen <feber.sm@gmail.com>
2016-01-23 23:35:11 +0100

    Fixed Comments

--- app/src/main/java/com/radionula/fragments/CommentsFragment.java
+++ app/src/main/java/com/radionula/fragments/CommentsFragment.java
@@ -5,19 +5,24 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Message;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.CookieManager;
+import android.webkit.CookieSyncManager;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
@@ -34,17 +39,49 @@ public class CommentsFragment extends Fragment {
     public static String BASE_DOMAIN = "http://radionula.com";
 
 
+    private WebView webView,childView =null;
+    private LinearLayout parentLayout;
+
     public CommentsFragment() {
         // Required empty public constructor
     }
 
-
+    @SuppressWarnings("deprecation")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view =  inflater.inflate(R.layout.fragment_comments, container, false);
+        View view = inflater.inflate(R.layout.fragment_comments, container, false);
 
+
+        parentLayout =(LinearLayout)view.findViewById(R.id.parentLayout);
+
+
+
+
+
+        webView = new WebView(getActivity());
+        webView.setLayoutParams(getLayoutParams());
+
+        webView.setWebViewClient(new FaceBookClient());
+        webView.setWebChromeClient(new MyChromeClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setAppCacheEnabled(true);
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        webView.getSettings().setSupportMultipleWindows(true);
+        webView.getSettings().setSupportZoom(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            webView.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+            CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true);
+        }
+
+        parentLayout.addView(webView);
+
+
+
+/*
         myWebView = (WebView) view.findViewById(R.id.fragment_comments_webview);
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
@@ -52,13 +89,14 @@ public class CommentsFragment extends Fragment {
         webSettings.setSupportMultipleWindows(true);
         webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
 
-        //myWebView.setWebViewClient(new WebViewClient());
 
+
+        myWebView.setWebViewClient(new WebViewClient());
 
         myWebView.setWebChromeClient(new WebChromeClient() {
             @Override
             public boolean onCreateWindow(WebView view, boolean isDialog, boolean isUserGesture, Message resultMsg) {
-               // myWebView.removeAllViews();
+                // myWebView.removeAllViews();
 
                 WebView newView = new WebView(getContext());
                 newView.setWebViewClient(new WebViewClient());
@@ -76,21 +114,89 @@ public class CommentsFragment extends Fragment {
 
 
         });
-
-        myWebView.loadDataWithBaseURL(BASE_DOMAIN,
-                "<html><head></head><body><div id=\"fb-root\"></div><div id=\"fb-root\"></div><script>(function(d, s, id) {var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) return;js = d.createElement(s); js.id = id;js.src = \"http://connect.facebook.net/en_US/all.js#xfbml=1&appId=" + APP_KEY + "\";fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'facebook-jssdk'));</script><div class=\"fb-comments\" data-href=\""
-                        + BASE_DOMAIN + "\" data-width=\"470\" data-order-by=\"reverse_time\"></div> </body></html>", "text/html", null, null);
+*/
+        webView.loadDataWithBaseURL(BASE_DOMAIN,
+                "<html>" +
+                        "<head></head>" +
+                        "<body>" +
+                        "<div id=\"fb-root\"></div><div id=\"fb-root\"></div>" +
+                        "<script>" +
+                        "(function(d, s, id) {var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) return;js = d.createElement(s); js.id = id;js.src = \"http://connect.facebook.net/en_US/all.js#xfbml=1&appId=" + APP_KEY + "\";fjs.parentNode.insertBefore(js, fjs);}" +
+                        "(document, 'script', 'facebook-jssdk'));" +
+                        "</script>" +
+                        "<div class=\"fb-comments\" data-href=\"" + BASE_DOMAIN + "\" data-width=\"470\" data-order-by=\"reverse_time\" data-colorscheme=\"light\"></div> " +
+                        " " +
+                        " </body>" +
+                        "</html>", "text/html", null, null);
 
         return view;
     }
 
-    public boolean webViewGoBack(){
-        if (myWebView.canGoBack())
-        {
-            myWebView.goBack();
+    public boolean webViewGoBack() {
+        if(childView != null && parentLayout.getChildCount()==2){
+            childView.stopLoading();
+            parentLayout.removeViewAt(parentLayout.getChildCount()-1);
+            if(webView.getVisibility() == View.GONE)
+                webView.setVisibility(View.VISIBLE);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
+    private LinearLayout.LayoutParams getLayoutParams(){
+        return new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT,LinearLayout.LayoutParams.FILL_PARENT);
+    }
+
+
+    final class MyChromeClient extends WebChromeClient{
+        @Override
+        public boolean onCreateWindow(WebView view, boolean dialog,
+                                      boolean userGesture, Message resultMsg) {
+            childView = new WebView(getActivity());
+            childView.getSettings().setJavaScriptEnabled(true);
+            childView.getSettings().setSupportZoom(true);
+            childView.getSettings().setBuiltInZoomControls(true);
+            childView.setWebViewClient(new FaceBookClient());
+            childView.setWebChromeClient(this);
+            childView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT,LinearLayout.LayoutParams.FILL_PARENT));
+
+
+            parentLayout.addView(childView);
+
+
+            childView.requestFocus();
+            webView.setVisibility(View.GONE);
+
+          /*I think this is the main part which handles all the log in session*/
+            WebView.WebViewTransport transport =(WebView.WebViewTransport)resultMsg.obj;
+            transport.setWebView(childView);
+            resultMsg.sendToTarget();
             return true;
         }
-        return false;
+
+
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            getActivity().setProgress(newProgress * 100);
+        }
+
+        @Override
+        public void onCloseWindow(WebView window) {
+            parentLayout.removeViewAt(parentLayout.getChildCount()-1);
+            childView =null;
+            webView.setVisibility(View.VISIBLE);
+            webView.requestFocus();
+        }
+    }
+
+    private class FaceBookClient extends WebViewClient{
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            Log.i("REQUEST URL", url);
+            return false;
+        }
     }
 
 
