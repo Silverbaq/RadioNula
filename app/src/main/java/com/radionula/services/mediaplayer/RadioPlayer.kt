bed5ecbe1967073482fecfeac14a78d15cb35695
Steffen Mogensen <feber.sm@gmail.com>
2021-07-03 20:21:51 +0200

    Updated ExoPlayer

--- app/src/main/java/com/radionula/services/mediaplayer/RadioPlayer.kt
+++ app/src/main/java/com/radionula/services/mediaplayer/RadioPlayer.kt
@@ -3,79 +3,23 @@ package com.radionula.services.mediaplayer
 import android.content.Context
 import android.media.MediaPlayer
 import android.net.Uri
-import android.util.Log
-import com.google.android.exoplayer2.*
-import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory
-import com.google.android.exoplayer2.source.ExtractorMediaSource
-import com.google.android.exoplayer2.source.TrackGroupArray
-import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
-import com.google.android.exoplayer2.trackselection.TrackSelectionArray
-import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter
-import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
-import com.google.android.exoplayer2.util.Util
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.SimpleExoPlayer
 import com.radionula.radionula.R
-
 
 class RadioPlayer(private val context: Context) {
 
     private val mpNoize: MediaPlayer = MediaPlayer.create(context, R.raw.radionoise)
-    private lateinit var exoPlayer: ExoPlayer
+    private val exoPlayer: SimpleExoPlayer = SimpleExoPlayer.Builder(context).build();
 
     init {
         mpNoize.isLooping = true
     }
 
     private fun prepareExoPlayerFromURL(uri: Uri) {
-
-        val trackSelector = DefaultTrackSelector()
-        val loadControl = DefaultLoadControl()
-
-        exoPlayer = ExoPlayerFactory.newSimpleInstance(context, trackSelector, loadControl)
-
-        val bandwidthMeter = DefaultBandwidthMeter()
-        val dataSourceFactory = DefaultDataSourceFactory(context, Util.getUserAgent(context, "exoplayer2example"), bandwidthMeter)
-        val extractorsFactory = DefaultExtractorsFactory()
-        val audioSource = ExtractorMediaSource(uri, dataSourceFactory, extractorsFactory, null, null)
-        exoPlayer.addListener(eventListener)
-
-        exoPlayer.prepare(audioSource)
-    }
-
-
-    private val eventListener = object : ExoPlayer.EventListener {
-        override fun onTimelineChanged(timeline: Timeline?, manifest: Any?) {
-            //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        }
-
-
-        override fun onTracksChanged(trackGroups: TrackGroupArray, trackSelections: TrackSelectionArray) {}
-
-        override fun onLoadingChanged(isLoading: Boolean) {
-            Log.i(TAG, "onLoadingChanged")
-        }
-
-        override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
-            Log.i(TAG, "onPlayerStateChanged: playWhenReady = " + playWhenReady.toString()
-                    + " playbackState = " + playbackState)
-            when (playbackState) {
-                ExoPlayer.STATE_ENDED -> {
-                    //Stop playback and return to start position
-                    //setPlayPause(false)
-                    exoPlayer.seekTo(0)
-                }
-                ExoPlayer.STATE_READY -> stopRadioNoize()
-                ExoPlayer.STATE_BUFFERING -> Log.i(TAG, "Playback buffering!")
-                ExoPlayer.STATE_IDLE -> Log.i(TAG, "ExoPlayer idle!")
-            }
-        }
-
-        override fun onPlayerError(error: ExoPlaybackException) {
-            Log.i(TAG, "onPlaybackError: " + error.message)
-        }
-
-        override fun onPositionDiscontinuity() {
-            Log.i(TAG, "onPositionDiscontinuity")
-        }
+        val mediaItem: MediaItem = MediaItem.fromUri(uri)
+        exoPlayer.setMediaItem(mediaItem)
+        exoPlayer.prepare()
     }
 
     fun tuneIn(radioUrl: String) {
@@ -86,24 +30,24 @@ class RadioPlayer(private val context: Context) {
         playRadio()
     }
 
-    fun playRadio(){
+    fun playRadio() {
         exoPlayer.playWhenReady = true
+        mpNoize.stop()
     }
 
-    fun pauseRadio(){
+    fun pauseRadio() {
         exoPlayer.playWhenReady = false
     }
 
-    fun startRadioNoize(){
+    fun startRadioNoize() {
         mpNoize.start()
     }
 
-    fun stopRadioNoize(){
+    fun stopRadioNoize() {
         mpNoize.stop()
     }
 
     companion object {
         const val TAG = "RadioPlayer"
     }
-
 }
